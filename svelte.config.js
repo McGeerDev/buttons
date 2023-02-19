1e947f688bd2cea2666dcac80cacc29b5866b0ba
mcgeerdev <mcgeer.devan@gmail.com>
2023-02-19 19:46:42 +0200

    Added svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,23 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: (css) => { 
+        css.write('public/build/bundle.css')
+      }
+    })
+  ]
+  
 }
